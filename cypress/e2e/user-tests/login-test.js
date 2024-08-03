a34f47eda15395c1b678be911ddd9778cba71fd6
husnadskrn <husnadskrn@outlook.com>
2024-08-03 21:46:05 +0300

    Add to login test cases

--- cypress/e2e/user-tests/login-test.js
+++ cypress/e2e/user-tests/login-test.js
@@ -1,7 +1,21 @@
 /// <reference types="cypress" />
 
 describe('Login Test', () => {
-      it('Login Successfull', () => {
-        cy.visit('https://www.netflix.com/login')
+      it('Login Successful', () => {
+        cy.visit('https://www.netflix.com/login');
+        cy.get("[id='onetrust-accept-btn-handler']").click();
+        cy.get("[name='userLoginId']").type("husnadskrntest@gmail.com");
+        cy.get("[data-uia='password-field']").type("deneme6767..hh");
+        cy.get("[data-uia='login-submit-button']").click();
+        cy.get("[id='signOut']").should('have.text', "Oturumu Kapat");
+      })
+      
+      it('Login Unsuccesful', () => {
+        cy.visit('https://www.netflix.com/login');
+        cy.get("[id='onetrust-accept-btn-handler']").click();
+        cy.get("[name='userLoginId']").type("husnadskrntest@gmail.com");
+        cy.get("[data-uia='password-field']").type("6767..hh");
+        cy.get("[data-uia='login-submit-button']").click();
+        cy.get("[data-uia='error-message-container+header']").should('have.text', "Incorrect password for husnadskrntest@gmail.com");
       })
     })
